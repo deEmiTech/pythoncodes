588efdfe43fa7ac66a584a0f4449bcac9b12ff3c
deEmiTech <deemitech@gmail.com>
2020-08-02 00:21:48 -0300

    funcionando
    funcionando mas falta design e texto

--- jokenpo.py
+++ jokenpo.py
@@ -8,12 +8,20 @@ jog = int(input('''ja escolhi, agora e sua vez.
 	[3] tesoura
 	[sua opcao]'''))
 
-
 if jog == 1:
 	jog = 'pedra'
 elif jog == 2:
 	jog = 'papel'
 elif jog == 3:
-	jog = 'tesoura'	
-print('computador:',comp,'jogador',jog)
+	jog = 'tesoura'
+
+if jog == comp:
+	win = 'empate'
+elif jog =='pedra' and comp == 'tesoura' or jog == 'papel' and comp == 'pedra' or jog == 'tesoura' and comp == 'papel':
+	win = 'jogador'
+elif comp =='pedra' and jog == 'tesoura' or comp == 'papel' and jog == 'pedra' or comp == 'tesoura' and jog == 'papel':
+	win = 'computador'	
+print('computador:',comp,'jogador:',jog)
 sleep(1)
+print(win)
+sleep(3)
