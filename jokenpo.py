5febbc9e027bb4b533f3b6d0177c90ebc4f04828
deEmiTech <deemitech@gmail.com>
2020-08-02 00:00:50 -0300

    update options
    opcao (pedra,papel ou tesoura) do comp e do jog

--- jokenpo.py
+++ jokenpo.py
@@ -1,9 +1,19 @@
-from random import choise
+from random import choice
 from time import sleep
 
-comp = choise([])
+comp = choice(['pedra', 'papel', 'tesoura'])
 jog = int(input('''ja escolhi, agora e sua vez.
 	[1] pedra
 	[2] papel
 	[3] tesoura
 	[sua opcao]'''))
+
+
+if jog == 1:
+	jog = 'pedra'
+elif jog == 2:
+	jog = 'papel'
+elif jog == 3:
+	jog = 'tesoura'	
+print('computador:',comp,'jogador',jog)
+sleep(1)
